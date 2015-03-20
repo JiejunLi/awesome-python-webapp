9abc584e8b03a473023f85a1a5d9a9e61abeb7a3
AlljunLee <1126216088@qq.com>
2015-03-20 10:36:21 +0800

    define web framework in file www/transwarp/web.py
    Signed-off-by: AlljunLee <1126216088@qq.com>

---
+++ www/transwarp/web.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+A simple, lightweight, WSGI-compatible web framework.
+'''
+
+__author__ = 'Alljun Lee'
+
+import sys	
+
+
+
+
+
+
+if __name__ == '__main__':
+    sys.path.append('.')
+    import doctest
+    doctest.testmod()
