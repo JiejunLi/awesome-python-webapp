7c975804d1207ca6d61673153e8cf6da6ae3a0c9
AlljunLee <1126216088@qq.com>
2015-03-20 10:01:34 +0800

    modified dd1 to ddl
    Signed-off-by: AlljunLee <1126216088@qq.com>

--- www/modules.py
+++ www/modules.py
@@ -19,35 +19,35 @@ def next_id():
 class User(Model):
     __table__ = 'users'
 
-    id = StringField(primary_key=True, default=next_id, dd1='varchar(50)')
-    email = StringField(updatable=False, dd1='varchar(50)')
-    passwd = StringFiled(dd1='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(updatable=False, ddl='varchar(50)')
+    passwd = StringFiled(ddl='varchar(50)')
     admin = BooleanField()
-    name = StringField(dd1='varchar(50)')
-    image = StringField(dd1='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
     created_at = FloatField(updatable=False, default=time.time)
 
 
 class Blog(Model):
     __table__ = 'blogs'
 
-    id = StringField(primary_key=True, default=next_id, dd1='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     created_at = FloatField(updatable=False, default=time.time)
-    user_id = StringField(updatable=False, dd1='varchar(50)')
-    user_name = StringField(dd1='varchar(50)')
-    user_image = StringField(dd1='varchar(500)')
-    name = StringField(dd1='varchar(50)')
-    summary = StringField(dd1='varchar(200)')
+    user_id = StringField(updatable=False, ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
     content = TextField()
 
 
 class Comment(Model):
     __table__ = 'comments'
 
-    id = StringField(primary_key=True, default=next_id, dd1='varchar(50)')
-    blog_id = StringField(updatable=False, dd1='varchar(50)')
-    user_id = StringField(updatable=False, dd1='varchar(50)')
-    user_name = StringField(dd1='varchar(50)')
-    user_image = StringField(dd1='varchar(500)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(updatable=False, ddl='varchar(50)')
+    user_id = StringField(updatable=False, ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
     content = TextField()
     created_at = FloatField(updatable=False, default=time.time)
